86bc2fd8f676e13de930df3bc677217e21dbcf5e
Michael Sloma <msloma@xometry.com>
2019-11-17 14:09:30 -0500

    add some routes

--- padkp_api/urls.py
+++ padkp_api/urls.py
@@ -3,9 +3,8 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-#router.register(r'awards', views.DkpAwardViewSet)
-#router.register(r'purchases', views.PurchaseViewSet)
 router.register(r'characters', views.CharacterViewSet)
+router.register(r'awards', views.DkpSpecialAwardViewSet)
 router.register(r'upload_dump', views.UploadRaidDump)
 router.register(r'charge_dkp', views.ChargeDKP)
 
