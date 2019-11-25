ba5a759f212e85d76be31567ba3fd708bfc4a603
Michael Sloma <msloma@xometry.com>
2019-11-25 06:56:18 -0500

    add item and award page skeletons

--- padkp_show/urls.py
+++ padkp_show/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     # ex: /polls/5/
     path('attendance/', views.attendance_table, name='dkp'),
+    path('awards/', views.awards, name='awards'),
+    path('items/', views.items, name='items'),
     path('<slug:character>/', views.character_dkp, name='dkp'),
 ]
--- padkp_show/views.py
+++ padkp_show/views.py
@@ -104,3 +104,24 @@ def character_dkp(request, character):
     return HttpResponse(template.render(context, request))
 
 
+def items(request):
+    template = loader.get_template('padkp_show/items.html')
+
+    dumps = RaidDump.objects.values('characters_present').annotate(value=Sum('value'))
+    total_earned = {x['characters_present']: x['value'] for x in dumps}
+
+    purchases = Purchase.objects.values('character').annotate(value=Sum('value'))
+    result = []
+
+    return HttpResponse(template.render({'records': result}, request))
+
+
+def awards(request):
+    template = loader.get_template('padkp_show/awards.html')
+
+    dumps = RaidDump.objects.values('characters_present').annotate(value=Sum('value'))
+    total_earned = {x['characters_present']: x['value'] for x in dumps}
+    result = []
+
+    return HttpResponse(template.render({'records': result}, request))
+
