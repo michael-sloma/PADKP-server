9560b725cb71fc257e0fcf6940f73ccd9750a749
Robert Uhl <rcuhljr@gmail.com>
2020-12-07 22:57:31 -0500

    merge split raids

--- README.md
+++ README.md
@@ -7,6 +7,10 @@ Simple DKP display and CRUD operations
 * Install Python 3.7 and pip.
 * python -m pip install Django
 * python -m pip install djangorestframework
+* for VSCode linting:
+  * pip install pylint-django
+  * preferences -> "python.linting.pylintArgs": [ "--load-plugins=pylint_django" ]
+
 * from powershell or other tty (gitbash doesn't play nice here)
   * py manage.py migrate
   * py manage.py createsuperuser (provide username, email, and password for a local admin account)
--- padkp_api/views.py
+++ padkp_api/views.py
@@ -82,10 +82,18 @@ class UploadRaidDump(viewsets.ViewSet):
         notes = request.data['notes']
         award_type = request.data['award_type']
 
-        dump = models.RaidDump(value=value, attendance_value=attendance_value, filename=filename,
-                               time=time, notes=notes, award_type=award_type)
-        dump.save()
-        dump.characters_present.set(characters_present)
+        dump_query = models.RaidDump.objects.filter(time=time)
+
+        if len(dump_query) == 1:
+            dump = dump_query.get()
+            dump.characters_present.add(characters_present)
+        else:
+            dump = models.RaidDump(value=value, attendance_value=attendance_value,
+                                   filename=filename, time=time, notes=notes,
+                                   award_type=award_type)
+            dump.save()
+            dump.characters_present.set(characters_present)
+
         return Response('Raid dump upload successful', status=status.HTTP_201_CREATED)
 
 
