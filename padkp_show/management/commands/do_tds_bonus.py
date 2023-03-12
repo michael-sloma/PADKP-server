0e880bfb5b7f8ddd840ad4b473d49b77d4cd032c
Robert Uhl <rcuhljr@gmail.com>
2023-03-12 19:45:32 -0400

    TDS bonus command

---
+++ padkp_show/management/commands/do_tds_bonus.py
@@ -0,0 +1,18 @@
+import datetime as dt
+from padkp_show.models import Character, RaidDump
+
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    help = 'Perform TDS 20 dkp bonus'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        for character in Character.objects.all():
+            bonus = int(character.attendance(30)/5)
+            character.cap_alt_dkp(500, dry_run=False)
+            if bonus > 0:
+                character.give_bonus(bonus, 'TDS Bonus: 20 dkp max based on 30 day attendance', dry_run=False)
