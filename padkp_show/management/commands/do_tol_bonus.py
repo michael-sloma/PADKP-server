dd2496ede875ed12f6bb1f9ee19928b7aee419e4
Robert Uhl <rcuhljr@gmail.com>
2024-06-26 18:59:15 -0400

    tol bonus command

---
+++ padkp_show/management/commands/do_tol_bonus.py
@@ -0,0 +1,20 @@
+import datetime as dt
+from padkp_show.models import Character, RaidDump
+
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    help = 'Perform ToL 25 dkp bonus, and apply final DKP Caps'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        for character in Character.objects.all():
+            attendance = character.attendance(30)
+            bonus = int(attendance*40/100)
+            character.cap_alt_dkp(500, dry_run=False)
+            character.cap_dkp(300, "ToL Cap", dry_run=False)
+            if bonus > 0:
+                character.give_bonus(bonus, 'ToL: 40 dkp max based on 30 day attendance', dry_run=False)
